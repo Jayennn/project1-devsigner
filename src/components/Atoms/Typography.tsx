241058bec5e12eab1a5c0541c983d65dd2dea1b1
f0rtun-4 <sangadpintar@gmail.com>
2023-08-15 20:47:46 +0800

    fix: typography component error eslint

--- src/components/Atoms/Typography.tsx
+++ src/components/Atoms/Typography.tsx
@@ -12,54 +12,55 @@ type TypographyProps<T extends React.ElementType> = {
    children: React.ReactNode
 } & React.ComponentPropsWithoutRef<T>
 
-type TypographyComponent<T extends React.ElementType = 'p'> = React.FC<
-   TypographyProps<T>
->
+type TypographyComponent = <T extends React.ElementType = 'p'>(
+   props: TypographyProps<T>
+) => React.ReactElement | null;
+
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore aman aja bang
 
 const Typography: TypographyComponent = React.forwardRef(
-   <T extends React.ElementType = 'p'>(
+   <T extends React.ElementType = "p">(
       {
          as,
-         variant = 'p',
-         color = 'primary',
+         variant = "p",
+         color = "primary",
          className,
          children,
          ...props
       }: TypographyProps<T>,
-      ref?: React.ComponentPropsWithRef<T>['ref']
+      ref?: React.ComponentPropsWithRef<T>["ref"],
    ): React.ReactElement | null => {
-      const Component = as || 'p'
+      const Component = as || "p";
       return (
          <Component
             ref={ref}
             className={cn(
                [
-                  variant === 'h1' && ['text-4xl font-bold md:text-5xl'],
-                  variant === 'h2' && ['text-3xl font-bold md:text-4xl'],
-                  variant === 'h3' && [
-                     'text-2xl font-semibold md:text-3xl',
-                  ],
-                  variant === 'h4' && ['text-xl font-bold md:text-2xl'],
-                  variant === 'h5' && ['text-lg font-bold md:text-xl'],
-                  variant === 'h6' && ['text-base font-bold md:text-xl'],
-                  variant === 'p' && ['text-sm font-normal'],
+                  variant === "h1" && ["text-4xl md:text-4xl font-bold"],
+                  variant === "h2" && ["leading-5 text-3xl md:text-4xl font-bold"],
+                  variant === "h3" && ["leading-4 text-2xl md:text-3xl font-bold"],
+                  variant === "h4" && ["leading-3 text-xl md:text-2xl font-bold"],
+                  variant === "h5" && ["text-lg md:text-xl font-bold"],
+                  variant === "h6" && ["text-xl md:text-lg font-bold"],
+                  variant === "p" && ["text-sm md:text-base font-normal"],
                ],
                [
-                  color === 'primary' && ['text-black'],
-                  color === 'secondary' && ['text-black opacity-70'],
-                  color === 'white' && ['text-white'],
-                  color === 'blue' && ['text-themeBlue'],
+                  color === "primary" && ["text-[#2E3135]"],
+                  color === "secondary" && ["text-[#2E3135] opacity-70"],
+                  color === "white" && ["text-white"],
+                  color === "blue" && ["text-themeBlue"]
                ],
-               className
+               className,
             )}
             {...props}
          >
             {children}
          </Component>
-      )
-   }
-)
+      );
+   },
+);
 
-Typography.displayName = 'Typography'
 
-export { Typography }
+export default Typography;
